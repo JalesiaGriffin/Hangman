1565917485ce67a36266e8575011c73d3d6cccc5
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-11-08 18:59:44 -0500

    created word selection and board

--- src/main/java/learn/the/part/Main.java
+++ src/main/java/learn/the/part/Main.java
@@ -1,2 +1,87 @@
-package learn.the.part;public class Main {
+package learn.the.part;
+
+public class Main {
+    public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
+            "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+            "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+            "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+            "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon",
+            "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
+            "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+            "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+            "wombat", "zebra"};
+
+    public static String[] gallows = {"+---+\n" +
+            "|   |\n" +
+            "    |\n" +
+            "    |\n" +
+            "    |\n" +
+            "    |\n" +
+            "=========\n",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========\n",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "|   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|   |\n" +
+                    "     |\n" +
+                    "     |\n" +
+                    " =========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" + //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
+                    "     |\n" +
+                    "     |\n" +
+                    " =========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" +
+                    "/    |\n" +
+                    "     |\n" +
+                    " =========\n",
+
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" +
+                    "/ \\  |\n" +
+                    "     |\n" +
+                    " =========\n"
+    };
+
+    public static void main(String[] args) {
+        // Select word
+        int random = (int)(Math.random() * (words.length - 1));
+        String word = words[random];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            sb.append("_");
+        }
+        String placeholders = sb.toString();
+
+        // Print board
+        System.out.println(gallows[0]);
+        System.out.println("Word: " + placeholders);
+        System.out.println("Misses: ");
+        System.out.println("Guess: ");
+    }
 }
